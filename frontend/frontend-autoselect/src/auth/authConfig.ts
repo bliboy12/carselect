e8e2a13a2217120e3af01550c22ec36c25556b0c
Ali laptop <aliabdi7575@hotmail.com>
2026-05-27 04:32:19 +0200

    fix: more wrong url fixes

--- frontend/frontend-autoselect/src/auth/authConfig.ts
+++ frontend/frontend-autoselect/src/auth/authConfig.ts
@@ -1,8 +1,8 @@
 export const authConfig = {
     authority: "https://carselect-identityserver-evafhmh8eacxbbgd.westeurope-01.azurewebsites.net",
     client_id: "carselect-frontend",
-    redirect_uri: "https://carselect-api-cmbgbafrdqhebmgz.westeurope-01.azurewebsites.net/callback",
-    post_logout_redirect_uri: "https://carselect-api-cmbgbafrdqhebmgz.westeurope-01.azurewebsites.net",
+    redirect_uri: "https://pg3alicarselect.z6.web.core.windows.net/callback",
+    post_logout_redirect_uri: "https://pg3alicarselect.z6.web.core.windows.net",
     scope: "openid profile carselect.api.read carselect.api.write roles",
     response_type: "code",
     loadUserInfo: true,
